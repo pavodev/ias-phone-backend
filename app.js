0e65819afc0cf2d7f5b4edc2cf34bab985d8d8a3
Ivan Pavic <ivan96jp@gmail.com>
2023-01-25 22:28:34 +0100

    implemented sendmail logic with token check

--- app.js
+++ app.js
@@ -1,17 +1,80 @@
+require("dotenv").config();
+
 const express = require("express");
+const bp = require("body-parser");
+const cors = require("cors");
+const sgMail = require("@sendgrid/mail");
+const { createClient } = require("@supabase/supabase-js");
+
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.type('html').send(html));
+// Supabase
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_KEY
+);
 
+// Cors
+app.use(cors());
+
+// Body parser
+app.use(bp.json());
+app.use(bp.urlencoded({ extended: true }));
+
+// Listen
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
+// Other endpoints
+app.get("/", (req, res) => res.type("html").send(html));
+
+app.post("/sendmail", async (req, res) => {
+  // console.log(req.body);
+  // res.status(200);
+  // res.send();
+  const token = req.body.access_token;
+  const emailTo = req.body.email_to;
+  const emailFrom = req.body.email_from;
+  const emailSubject = req.body.email_subject;
+  const emailBody = req.body.email_body;
+
+  const { data, error } = await supabase.auth.getUser(token);
+
+  if (data.user) {
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    const msg = {
+      to: emailTo, // Change to your recipient
+      from: emailFrom, // Change to your verified sender
+      subject: emailSubject,
+      html: emailBody,
+    };
+
+    sgMail
+      .send(msg)
+      .then(() => {
+        console.log("Email sent");
+        res.json({ message: "E-mail sent!" });
+        res.status(200);
+        res.send();
+      })
+      .catch((error) => {
+        console.error(error);
+        res.json({ message: "The e-mail couldn't be sent.", error });
+        res.status(404);
+        res.send();
+      });
+  } else {
+    res.json({ message: "User invalid", user: data.user, valid: false });
+    res.status(403);
+    res.send();
+  }
+});
 
 const html = `
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Hello from Render!</title>
+    <title>IAS Phone Scheduler backend</title>
     <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
     <script>
       setTimeout(() => {
@@ -52,8 +115,8 @@ const html = `
   </head>
   <body>
     <section>
-      Hello from Render!
+      Hello from IAS Phone Scheduler backend!
     </section>
   </body>
 </html>
-`
+`;
